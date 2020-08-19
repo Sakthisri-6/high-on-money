d445f9e2cf4f0ae6964812dadd67093c87d11992
Sakthisri-6 <69898205+Sakthisri-6@users.noreply.github.com>
2020-08-19 03:13:57 -0700

    Uploaded my code for the first project
    It contains two files.one for to get the size of datatypes and other is to get a product of two number

---
+++ pro 1 que 2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	float a,b,x;
+	cout<<"enter first number..";
+	cin>>a;
+	cout<<"\nenter second number..";
+	cin>>b;
+	x = a*b;
+	cout<<"the product is..."<<x;
+	return 0;
+	
+}
---
+++ project 1 que1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int x;
+	char y;
+	float z;
+	double u;
+	cout<<"Size of char:"<<sizeof(y)<<"byte\n";
+	cout<<"Size of int:"<<sizeof(x)<<"bytes\n";
+	cout<<"Size of float:"<<sizeof(z)<<"bytes\n";
+	cout<<"Size of double:"<<sizeof(u)<<"bytes\n";
+	return 0;
+}
